7fe686f4495275235fe6a2683e308fd50f6ce090
Lewis Lakerink <llakerink@swin.edu.au>
2022-12-09 15:58:20 +1100

    Don't error if build dir already exists

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rustup component add llvm-tools-preview
 # Put WIFI_NETWORK and WIFI_PASSWORD in this file
 source .env
 
-mkdir build
+mkdir -p build
 cargo build --release
 cargo objcopy --release -- -O binary build/garagelight.bin
 uf2conv build/garagelight.bin --base 0x10000000 --family 0xe48bff56 --output build/garagelight.uf2

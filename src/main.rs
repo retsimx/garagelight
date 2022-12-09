388b2f6519aba5244bc9e038d26cc30fd22ed16f
Lewis Lakerink <llakerink@swin.edu.au>
2022-12-09 15:50:08 +1100

    Initial Rust project

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(async_fn_in_trait)]
+#![allow(incomplete_features)]
+
+mod wifi;
+
+use embassy_executor::Spawner;
+use embassy_net::{PacketMetadata};
+use embassy_net::udp::UdpSocket;
+use embassy_rp::gpio::{Flex, Level, Output};
+use {defmt_rtt as _, panic_probe as _};
+use crate::wifi::init_wifi;
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let peripherals = embassy_rp::init(Default::default());
+
+    let pwr = Output::new(peripherals.PIN_23, Level::Low);
+    let cs = Output::new(peripherals.PIN_25, Level::High);
+    let clk = Output::new(peripherals.PIN_29, Level::Low);
+    let dio = Flex::new(peripherals.PIN_24);
+
+    let stack = init_wifi(pwr, cs, clk, dio, spawner).await;
+
+    let mut rx_meta = [PacketMetadata::EMPTY; 16];
+    let mut rx_buffer = [0; 4096];
+    let mut tx_meta = [PacketMetadata::EMPTY; 16];
+    let mut tx_buffer = [0; 4096];
+    let mut buf = [0; 4096];
+
+    let mut socket = UdpSocket::new(stack, &mut rx_meta, &mut rx_buffer, &mut tx_meta, &mut tx_buffer);
+    socket.bind(8000).unwrap();
+
+    let mut led = Output::new(peripherals.PIN_28 , Level::High);
+
+    loop {
+        let (_n, _ep) = socket.recv_from(&mut buf).await.unwrap();
+
+        if buf[0] == 1 {
+            led.set_high()
+        }
+        else {
+            led.set_low()
+        }
+    }
+}
---
+++ src/wifi.rs
@@ -0,0 +1,162 @@
+use core::convert::Infallible;
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_net::{Stack, StackResources};
+use embassy_rp::gpio::{Flex, Output};
+use embassy_rp::peripherals::{PIN_23, PIN_24, PIN_25, PIN_29};
+use embedded_hal_1::spi::ErrorType;
+use embedded_hal_async::spi::{ExclusiveDevice, SpiBusFlush, SpiBusRead, SpiBusWrite};
+use static_cell::StaticCell;
+
+use {defmt_rtt as _, panic_probe as _};
+
+macro_rules! singleton {
+    ($val:expr) => {{
+        type T = impl Sized;
+        static STATIC_CELL: StaticCell<T> = StaticCell::new();
+        STATIC_CELL.init_with(move || $val)
+    }};
+}
+
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<'static, Output<'static, PIN_23>, ExclusiveDevice<MySpi, Output<'static, PIN_25>>>,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<cyw43::NetDevice<'static>>) -> ! {
+    stack.run().await
+}
+
+pub async fn init_wifi<'a>(
+    pwr: Output<'static, PIN_23>,
+    cs: Output<'static, PIN_25>,
+    clk: Output<'static, PIN_29>,
+    mut dio: Flex<'static, PIN_24>,
+    spawner: Spawner
+) -> &'static Stack<cyw43::NetDevice<'static>> {
+    // Include the WiFi firmware and Country Locale Matrix (CLM) blobs.
+    let fw = include_bytes!("../lib/cyw43/firmware/43439A0.bin");
+    let clm = include_bytes!("../lib/cyw43/firmware/43439A0_clm.bin");
+
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs-cli download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs-cli download 43439A0.clm_blob --format bin --chip RP2040 --base-address 0x10140000
+    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 224190) };
+    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+
+    dio.set_low();
+    dio.set_as_output();
+
+    let bus = MySpi { clk, dio };
+    let spi = ExclusiveDevice::new(bus, cs);
+
+    let state = singleton!(cyw43::State::new());
+    let (mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+
+    spawner.spawn(wifi_task(runner)).unwrap();
+
+    let net_device = control.init(clm).await;
+
+    //control.join_open(env!("WIFI_NETWORK")).await;
+    control.join_wpa2(env!("WIFI_NETWORK"), env!("WIFI_PASSWORD")).await;
+
+    let config = embassy_net::ConfigStrategy::Dhcp;
+    //let config = embassy_net::ConfigStrategy::Static(embassy_net::Config {
+    //    address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 69, 2), 24),
+    //    dns_servers: Vec::new(),
+    //    gateway: Some(Ipv4Address::new(192, 168, 69, 1)),
+    //});
+
+    // Generate random seed
+    let seed = 0x0123_4567_89ab_cdef; // chosen by fair dice roll. guarenteed to be random.
+
+    // Init network stack
+    let stack = &*singleton!(Stack::new(
+        net_device,
+        config,
+        singleton!(StackResources::<1, 2, 8>::new()),
+        seed
+    ));
+
+    unwrap!(spawner.spawn(net_task(stack)));
+
+    return stack;
+}
+
+struct MySpi {
+    /// SPI clock
+    clk: Output<'static, PIN_29>,
+
+    /// 4 signals, all in one!!
+    /// - SPI MISO
+    /// - SPI MOSI
+    /// - IRQ
+    /// - strap to set to gSPI mode on boot.
+    dio: Flex<'static, PIN_24>,
+}
+
+impl ErrorType for MySpi {
+    type Error = Infallible;
+}
+
+impl SpiBusFlush for MySpi {
+    async fn flush(&mut self) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+impl SpiBusRead<u32> for MySpi {
+    async fn read(&mut self, words: &mut [u32]) -> Result<(), Self::Error> {
+        self.dio.set_as_input();
+        for word in words {
+            let mut w = 0;
+            for _ in 0..32 {
+                w = w << 1;
+
+                // rising edge, sample data
+                if self.dio.is_high() {
+                    w |= 0x01;
+                }
+                self.clk.set_high();
+
+                // falling edge
+                self.clk.set_low();
+            }
+            *word = w
+        }
+
+        Ok(())
+    }
+}
+
+impl SpiBusWrite<u32> for MySpi {
+    async fn write(&mut self, words: &[u32]) -> Result<(), Self::Error> {
+        self.dio.set_as_output();
+        for word in words {
+            let mut word = *word;
+            for _ in 0..32 {
+                // falling edge, setup data
+                self.clk.set_low();
+                if word & 0x8000_0000 == 0 {
+                    self.dio.set_low();
+                } else {
+                    self.dio.set_high();
+                }
+
+                // rising edge
+                self.clk.set_high();
+
+                word = word << 1;
+            }
+        }
+        self.clk.set_low();
+
+        self.dio.set_as_input();
+        Ok(())
+    }
+}

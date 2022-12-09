1190f823a902fc44794babf2c4c0719ff0d83e21
Lewis Lakerink <llakerink@swin.edu.au>
2022-12-09 15:50:08 +1100

    Initial Rust project

---
+++ build.sh
@@ -0,0 +1,11 @@
+rustup target add thumbv6m-none-eabi
+cargo install uf2conv cargo-binutils
+rustup component add llvm-tools-preview
+
+# Put WIFI_NETWORK and WIFI_PASSWORD in this file
+source .env
+
+mkdir build
+cargo build --release
+cargo objcopy --release -- -O binary build/garagelight.bin
+uf2conv build/garagelight.bin --base 0x10000000 --family 0xe48bff56 --output build/garagelight.uf2

3ba8fda81ddb1fab0788d0baa8665f5f1eec5be0
Lewis Lakerink <llakerink@swin.edu.au>
2024-02-18 16:36:09 +1100

    Move GATT server to light

--- ble.py
+++ ble.py
@@ -1,68 +1,64 @@
 import sys
 
+from micropython import const
+
 sys.path.append("")
 
 import uasyncio as asyncio
 import aioble
 import bluetooth
 
-import struct
-
 # org.bluetooth.service.environmental_sensing
 _ENV_SENSE_UUID = bluetooth.UUID(0x181A)
 # org.bluetooth.characteristic.temperature
 _ENV_SENSE_TEMP_UUID = bluetooth.UUID(0x2A6E)
+# org.bluetooth.characteristic.gap.appearance.xml
+_ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
+
+# How frequently to send advertising beacons.
+_ADV_INTERVAL_MS = 250_000
 
 
-# Helper to decode the temperature characteristic encoding (sint16, hundredths of a degree).
-def _decode_temperature(data):
-    return struct.unpack("<h", data)[0]
+# Register GATT server.
+temp_service = aioble.Service(_ENV_SENSE_UUID)
+temp_characteristic = aioble.Characteristic(
+    temp_service, _ENV_SENSE_TEMP_UUID, write=True, notify=True
+)
+aioble.register_services(temp_service)
 
 
-async def find_temp_sensor():
-    # Scan for 5 seconds, in active mode, with very low interval/window (to
-    # maximise detection rate).
-    async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
-        async for result in scanner:
-            # See if it matches our name and the environmental sensing service.
-            if result.name() == "mpy-temp" and _ENV_SENSE_UUID in result.services():
-                return result.device
-    return None
+async def control_task(connection, light_pin):
+    global send_file, recv_file, list_path
+
+    try:
+        with connection.timeout(None):
+            while True:
+                print("Waiting for write")
+                await temp_characteristic.written()
+                msg = temp_characteristic.read()
+
+                if msg[0]:
+                    light_pin.off()
+                else:
+                    light_pin.on()
+
+    except aioble.DeviceDisconnectedError:
+        return
 
 
 async def ble_task(light_pin):
     while True:
-        await asyncio.sleep_ms(1000)
-
         try:
-            device = await find_temp_sensor()
-            if not device:
-                print("Temperature sensor not found")
-                continue
+            async with await aioble.advertise(
+                _ADV_INTERVAL_MS,
+                name="glgt",
+                services=[_ENV_SENSE_UUID],
+                appearance=_ADV_APPEARANCE_GENERIC_THERMOMETER,
+            ) as connection:
+                print("Connection from", connection.device)
 
-            try:
-                print("Connecting to", device)
-                connection = await device.connect()
-            except asyncio.TimeoutError:
-                print("Timeout during connection")
-                continue
+                await control_task(connection, light_pin)
 
-            async with connection:
-                try:
-                    temp_service = await connection.service(_ENV_SENSE_UUID)
-                    temp_characteristic = await temp_service.characteristic(_ENV_SENSE_TEMP_UUID)
-                except asyncio.TimeoutError:
-                    print("Timeout discovering services/characteristics")
-                    continue
-
-                while True:
-                    beam_state = await temp_characteristic.read()
-                    if beam_state[0]:
-                        light_pin.off()
-                    else:
-                        light_pin.on()
-
-                    await asyncio.sleep_ms(5)
-        except Exception as e:
-            print(e)
-            pass
+                await connection.disconnected()
+        except:
+            await asyncio.sleep_ms(1000)

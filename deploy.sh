d69feba01ce7698f9587e95a8848140066ef2641
Lewis Lakerink <llakerink@swin.edu.au>
2025-08-08 06:29:58 +0000

    Use freebsd nginx

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 mkdir -p /tmp/firmware
-sshfs root@10.2:/jails/containers/nginx/usr/local/www/firmware /tmp/firmware
+sshfs root@10.0.1.5:/usr/local/www/firmware /tmp/firmware
 
 # Get old and new version numbers
 old_version=$(cat /tmp/firmware/garagelight/version)
